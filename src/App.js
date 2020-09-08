dd61ee20008d28a50e6e12a6997e88205646b3ef
Robert Laws <roblaws@me.com>
2020-09-08 14:20:02 +0300

    begin setup of sections

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Main, Header, Footer } from './sections';
 
 function App() {
-  return <div className='font-body'>App</div>;
+  return (
+    <div className='w-screen h-screen flex flex-col font-body'>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
